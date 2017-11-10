db723756ff05209669c9dcbf87311089c4b22206
Birgit <britt2609@hotmail.com>
2017-11-10 12:45:31 +0000

    first commit

---
+++ rail2.py
@@ -0,0 +1,98 @@
+import csv
+
+
+def CalculateScore(trajecten, criticalConnections):
+    """Function to compute the score"""
+
+    minutes = 0
+    trains = len(trajecten)
+    critical_connection = 0
+    critical_connections_past = []
+
+    for traject in trajecten:
+        minutes += int(traject[1])
+        for i in range(len(traject[0]) - 1):
+            if [traject[0][i], traject[0][i + 1]] in criticalConnections or [traject[0][i + 1], traject[0][i]] in criticalConnections:
+                if [traject[0][i], traject[0][i + 1]] not in critical_connections_past and [traject[0][i + 1], traject[0][i]] not in critical_connections_past:
+                    critical_connections_past.append([traject[0][i], traject[0][i + 1]])
+                    critical_connection += 1
+
+    percentage = critical_connection/len(criticalConnections) * 100
+    print(percentage)
+    print(minutes)
+    print(len(trajecten))
+    score = percentage*10000 - (trains*20 + minutes/100000)
+    return score
+
+def make_all_routes(graph, start, end, route=[], time=0):
+    """Function to get all routes between to stations, within 2 hours"""
+
+    # Add start to route.
+    route = route + [start]
+
+    # At the end, return the route.
+    if start == end:
+        return [[route, time]]
+
+    # If the route doesn't exist, return nothing.
+    elif not graph[start]:
+        return []
+    routes = []
+
+    # Check the route for every destination.
+    for destination in graph[start]:
+
+        # Do not pass the same station twice.
+        if destination[0] not in route:
+            # Ensure the route doesn't take longer than 2 hours.
+            duration = time + int(destination[1])
+            if duration < 120:
+                # Make the new routes for the further stations.
+                new_routes = make_all_routes(graph, destination[0], end, route, duration)
+                for new_route in new_routes:
+                    routes.append(new_route)
+    return routes
+
+
+def main():
+    # List to store stations.
+    graph = {}
+
+    # List to store critical stations.
+    critical_stations = []
+
+    # List to store the names of the stations.
+    stationnames = []
+    stationsfile = open('StationsHolland.csv', 'rt')
+    stations = csv.reader(stationsfile)
+    for station in stations:
+        stationnames.append(station[0])
+        graph[station[0]] = []
+        if station[3] == 'Kritiek':
+            critical_stations.append(station[0])
+    stationsfile.close()
+
+
+    critical_connections = []
+    # Add connections to the stations.
+    connecties = open('ConnectiesHolland.csv', 'rt')
+    directions = csv.reader(connecties)
+    for direction in directions:
+        graph[direction[0]].append([direction[1], direction[2]])
+        graph[direction[1]].append([direction[0], direction[2]])
+        if direction[0] in critical_stations or direction[1] in critical_stations:
+            critical_connections.append([direction[0], direction[1]])
+    connecties.close()
+
+
+    # Add all of the routes to the list paths, don't use a station twice.
+    paths = []
+    for i in range(len(stationnames)):
+        for j in range(i + 1, len(stationnames)):
+            paths.extend(make_all_routes(graph, stationnames[i], stationnames[j]))
+
+    print("Score: ", CalculateScore(paths, critical_connections))
+
+
+if __name__ == "__main__":
+    main()
