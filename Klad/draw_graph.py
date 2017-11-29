9e9ba28fe716609771f3f074a71c31043fb6c0f8
susannebink <s.binkhorst96@gmail.com>
2017-11-29 12:24:25 +0100

    figuur aangepast

--- Klad/draw_graph.py
+++ Klad/draw_graph.py
@@ -8,7 +8,10 @@ stationsCsvFile = 'C:/Users/Susanne/RailNL/csvFiles/StationsHolland.csv'
 stationsfile = open(stationsCsvFile, 'rt')
 stations = csv.reader(stationsfile)
 for station in stations:
-    G.add_node("" + station[0], pos = (float(station[2]), float(station[1])))
+    if station[3] == "Kritiek":
+        G.add_node("" + station[0], pos = (float(station[2]), float(station[1])), node_colour = "r")
+    else:
+        G.add_node("" + station[0], pos = (float(station[2]), float(station[1])), node_colour = "b")
 
 
 connectiesCsvFile = 'C:/Users/Susanne/RailNL/csvFiles/ConnectiesHolland.csv'
@@ -20,8 +23,9 @@ for direction in directions:
 print(labels)
 
 pos=nx.get_node_attributes(G,'pos')
+# node_colour=nx.get_node_attributes(G, 'node_colour')
 plt.figure(1, figsize = (10,10))
-nx.draw(G, pos, with_labels = True, node_size=70, font_size=9, label_pos=3)
+nx.draw(G, pos, with_labels = True, node_size=70, font_size=16, label_pos=3)
 nx.draw_networkx_edge_labels(G, pos, labels, alpha = 0.5)
 plt.savefig("plot.png")
-#plt.show()
+plt.show()
---
+++ Klad/giu.py
@@ -0,0 +1,27 @@
+print("For depth first algorithm, type: 1")
+print("For Dijkstra's algorithm, type: 2")
+print("For Sven's algorithm, type: 3")
+print("For the Hillclimber, type: 4")
+
+algorithm = input("Select: ")
+
+
+
+if (int(algorithm) == 1):
+
+elif (int(algorithm) == 2):
+
+elif (int(algorithm) == 3):
+
+elif (int(algorithm) == 4):
+    bestPaths = algoritm3(graph)
+    pathsSelected = bestPaths[0:7]
+
+    bestScore = CalculateScore(pathsSelected, graph.criticalConnections)
+    for i in range(100):
+        pathsSelected, bestScore = HillClimber(graph, pathsSelected, bestPaths, bestScore)
+    print("paths: ", pathsSelected)
+    print("bestScore: ", bestScore)
+else:
+    while (int(algorithm) != 1 or int(algorithm) != 2 or int(algorithm) != 3 or int(algorithm) != 4):
+         algorithm = input("Please select valid algorithm: ")
