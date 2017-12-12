9d0f241d225b2276830e8a5cfd42d078e5f60f81
suitendaal <sven.uitendaal1@gmail.com>
2017-12-12 15:47:46 +0100

    grafiek gefixt

--- PlotFunctions/make_graph.py
+++ PlotFunctions/make_graph.py
@@ -4,9 +4,18 @@ import numpy as np
 import os
 
 def makeGraph(file_name, fig_name):
-    plot = np.genfromtxt(file_name)
+    # Create Figure and Axes instances
+    fig,ax = plt.subplots(1)
 
+    plot = np.genfromtxt(file_name)
     x = np.arange(0, len(plot))
-    plt.plot(x, plot)
-    plt.ylabel("Score")
-    plt.savefig(fig_name)
+
+    # Make your plot, set your axes labels
+    ax.plot(x,plot,'k')
+    ax.set_ylabel('V')
+    ax.set_xlabel('t')
+
+    plt.show()
+
+    # plt.plot(x, plot)
+    # plt.ylabel("Score")
