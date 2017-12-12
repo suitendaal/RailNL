3c1d00d1b606e88fdc5fefa210720fa2176d825c
susannebink <s.binkhorst96@gmail.com>
2017-12-12 15:57:41 +0100

    commit

--- PlotFunctions/make_graph.py
+++ PlotFunctions/make_graph.py
@@ -9,19 +9,9 @@ def makeGraph(file_name, fig_name):
 
     plot = np.genfromtxt(file_name)
     x = np.arange(0, len(plot))
-<<<<<<< HEAD
 
     # Make your plot, set your axes labels
     ax.plot(x,plot,'k')
-    ax.set_ylabel('V')
-    ax.set_xlabel('t')
-
+    ax.set_ylabel('Score')
+    ax.set_xlabel('Iterations')
     plt.show()
-
-    # plt.plot(x, plot)
-    # plt.ylabel("Score")
-=======
-    plt.plot(x, plot)
-    # plt.ylabel("Score")
-    plt.savefig(fig_name)
->>>>>>> f0f67f2e9d0f61edbc5ec7c6e3af39ce55f92b7f
